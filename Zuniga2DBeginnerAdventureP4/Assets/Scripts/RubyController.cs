c61cd2d1298a3e072fbd06abd6da8457981571f5
matt <769234@student.dusd.net>
2025-11-06 15:23:09 -0800

    fixed movement code
    added y axis

--- Zuniga2DBeginnerAdventureP4/Assets/Scripts/RubyController.cs
+++ Zuniga2DBeginnerAdventureP4/Assets/Scripts/RubyController.cs
@@ -13,8 +13,13 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
         Vector2 position = transform.position;
         position.x = position.x + 0.1f;
+        position.y = position.y + 0.1f;
+
         transform.position = position;
     }
 }
